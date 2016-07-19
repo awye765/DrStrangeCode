94aa6813c0251703399e1f22283ec96bc06b1820
lomlo <michael.lo@live.co.uk>
2016-07-19 15:49:24 +0100

    added a one to many relationship between the user and snippets

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :user_name, presence: true, length: {minimum: 4, maximum: 16}
+
+  has_many :snippets, dependent: :destroy
 end
