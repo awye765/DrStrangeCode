d16f5d811588fd8e7b6fb4206c2839c41aedf722
lomlo <michael.lo@live.co.uk>
2016-07-19 17:01:12 +0100

    draft code to be pulled down

--- spec/features/editing_snippets_spec.rb
+++ spec/features/editing_snippets_spec.rb
@@ -2,48 +2,47 @@ require 'rails_helper'
 
 feature 'Editing posts' do
 
-  before do
-    user_one = create(:user)
-    user_two = create(:user, email: 'hi@hi.com',
-                             user_name: 'BennyBoy',
-                             id: user_one.id + 1)
+    before do
+      user_one = create(:user)
+      user_two = create(:user, email: 'hi@hi.com',
+                               user_name: 'BennyBoy',
+                               id: user_one.id + 1)
 
-    @snippet_one = create(:snippet, user_id: user_one.id)
-    @snippet_two = create(:snippet, user_id: user_one.id + 1)
+      @snippet_one = create(:snippet, user_id: user_one.id)
+      @snippet_two = create(:snippet, user_id: user_one.id + 1)
 
-    sign_in_with user_one
-  end
+      sign_in_with user_one
+    end
 
-  scenario 'can edit a snippet as the owner' do
-    find(:xpath, "//a[contains(@href,'snippets/#{@snippet_one.id}')]").click
-    expect(page).to have_content('Edit Post')
-​
-    click_link 'Edit Post'
-    fill_in 'Caption', with: "Oh god, you weren't meant to see this picture!"
-    click_button 'Update Post'
-​
-    expect(page).to have_content("Post updated hombre")
-    expect(page).to have_content("Oh god, you weren't meant to see this picture!")
-  end
+    scenario 'As the owner can edit a snippet as the owner' do
+      find(:xpath, "//a[contains(@href,'snippets/#{@snippet_one.id}')]").click
+      expect(page).to have_content('Edit')
 
-  scenario "cannot edit a post that doesn't belong to you via the show page" do
-    find(:xpath, "//a[contains(@href,'posts/#{@snippet_two.id}')]").click
-    expect(page).to_not have_content('Edit Post')
-  end
+      click_link 'Edit'
+      fill_in 'Name', with: "Airplane Challenge"
+      fill_in 'Code', with: "```<h1>Hello World</h1>```"
+      click_button 'Update Snippet'
 
-  scenario "cannot edit a post that doesn't belong to you via url path" do
-    visit "/posts/#{@snippet_two.id}/edit"
-    expect(page.current_path).to eq root_path
-    expect(page).to have_content("That post doesn't belong to you!")
-  end
+      expect(page).to have_content("Snippet was successfully updated")
+      expect(page).to have_content("<h1>Hello World</h1>")
+    end
 
-  scenario "It won't update a post without an attached image" do
-    find(:xpath, "//a[contains(@href,'posts/#{@snippet_one.id}')]").click
-    click_link 'Edit Post'
-    attach_file('Image', 'spec/files/coffee.zip')
-    click_button 'Update Post'
+    scenario "A stranger  cannot edit a snippet" do
+      find(:xpath, "//a[contains(@href,'snippets/#{@snippet_two.id}')]").click
+      expect(page).to_not have_content('Edit')
+    end
 
-    expect(page).to have_content("Something is wrong with your form!")
-  end
+    scenario "A stranger cannot edit a snippet that via url path" do
+      visit "/snippets/#{@snippet_two.id}/edit"
+      expect(page.current_path).to eq root_path
+      expect(page).to have_content("That post doesn't belong to you!")
+    end
 
+    scenario "It won't update a post without an attached image" do
+      find(:xpath, "//a[contains(@href,'snippets/#{@snippet_one.id}')]").click
+      click_link 'Edit Snippet'
+      click_button 'Update Snippet'
+
+      expect(page).to have_content("Something is wrong with your form!")
+    end
 end
