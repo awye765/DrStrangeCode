f18470f91e379433cb986c8bd52532de63c8fb66
Elena <elena.vilimaite@gmail.com>
2016-07-19 10:47:31 +0100

    devise login added

--- spec/features/Creating_new_user_spec.rb
+++ spec/features/Creating_new_user_spec.rb
@@ -1,82 +1,73 @@
 require 'rails_helper'
-​
+
 feature "Creating a new user" do
-​
   before do
-    visit '/'
-    click_link "Register"
+    visit '/users/sign_up'
+    # click_link "Register"
   end
-​
   scenario "can create a new user via the index page" do
     fill_in 'Email', with: 'syxrailsdev@gmail.com'
     fill_in 'User name', with: 'sxyrailsdev'
     fill_in 'Password', with: 'password', match: :first
     fill_in 'Password confirmation', with: 'password'
-​
+
     click_button 'Sign up'
     expect(page).to have_content 'Welcome! You have signed up successfully'
   end
-​
+
   scenario "requires a user name to successfully create an account" do
     fill_in 'Email', with: 'syxrailsdev@gmail.com'
     fill_in 'Password', with: 'password', match: :first
     fill_in 'Password confirmation', with: 'password'
-​
+
     click_button 'Sign up'
     expect(page).to have_content("can't be blank")
   end
-​
+
   scenario "requires a user name to be more than 4 characters" do
     fill_in 'Email', with: 'syxrailsdev@gmail.com'
     fill_in 'User name', with: 'bob'
     fill_in 'Password', with: 'password', match: :first
     fill_in 'Password confirmation', with: 'password'
-​
+
     click_button 'Sign up'
     expect(page).to have_content("is too short (minimum is 4 characters)")
   end
-​
+
   scenario "requires a user name to be less than 4 characters" do
     fill_in 'Email', with: 'syxrailsdev@gmail.com'
     fill_in 'User name', with: 'bobbbbbbbbbbbbbbbbbbbbbbbbb'
     fill_in 'Password', with: 'password', match: :first
     fill_in 'Password confirmation', with: 'password'
-​
+
     click_button 'Sign up'
     expect(page).to have_content("is too long (maximum is 16 characters)")
   end
-​
+
   scenario "requires a password" do
     fill_in 'Email', with: 'syxrailsdev@gmail.com'
     fill_in 'User name', with: 'bobby'
     fill_in 'Password confirmation', with: 'password'
-​
+
     click_button 'Sign up'
     expect(page).to have_content("can't be blank")
-​
   end
-​
   scenario "requires a password to be 6 characters or more" do
     fill_in 'Email', with: 'syxrailsdev@gmail.com'
     fill_in 'User name', with: 'bobby'
     fill_in 'Password confirmation', with: 'pass'
     fill_in 'Password confirmation', with: 'password'
-​
+
     click_button 'Sign up'
     expect(page).to have_content("6 characters minimum")
-​
   end
-​
   scenario "requires a password to be 6 characters or more" do
     fill_in 'Email', with: 'syxrailsdev@gmail.com'
     fill_in 'User name', with: 'bobby'
     fill_in 'Password confirmation', with: 'password1'
     fill_in 'Password confirmation', with: 'password2'
-​
+
     click_button 'Sign up'
     expect(page).to have_content("doesn't match Password")
-​
   end
-​
-​
 end
