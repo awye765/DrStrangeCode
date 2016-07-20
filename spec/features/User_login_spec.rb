e93a6295aec06cad4648ef1a711578544392af4d
lomlo <michael.lo@live.co.uk>
2016-07-20 12:15:51 +0100

    refactored the user_login_spec - added context for users without an account

--- spec/features/User_login_spec.rb
+++ spec/features/User_login_spec.rb
@@ -2,37 +2,51 @@ require 'rails_helper'
 
 feature 'User authentication' do
 
-  before do
-    @user = create(:user)
-    @snippet = create(:snippet, name: 'Test', code: 'Test code', user_id: @user.id)
+  context 'A user with an account' do
+    scenario "can log in " do
+      @user = create(:user)
+
+      sign_in_with @user
+
+      expect(page).to have_content('Signed in successfully.')
+      expect(page).to_not have_content('Sign up')
+      expect(page).to have_content('Log out')
+    end
+
+    scenario 'can log out once logged in' do
+      @user = create(:user)
+
+      sign_in_with @user
+      click_link 'Log out'
+
+      expect(page).to have_content('Signed out successfully')
+    end
+
+    scenario 'can view index posts without logging in' do
+      @user = create(:user)
+      @snippet = create(:snippet, name: 'Test', code: 'Test code', user_id: @user.id)
+
+      visit '/'
+
+      expect(page).to have_content('Test')
+    end
+
+    scenario 'cannot create a new post without logging in' do
+      @user = create(:user)
+
+      visit '/'
+
+      expect(page).not_to have_content('Add a Snippet')
+    end
   end
 
-  scenario "can log in from the index" do
-    visit '/'
-    expect(page).to_not have_content('New Post')
+  context 'A user without an account' do
+    scenario "can not log in " do
+      @user = build(:user)
 
-    sign_in_with @user
+      sign_in_with @user
 
-    expect(page).to have_content('Signed in successfully.')
-    expect(page).to_not have_content('Sign up')
-    expect(page).to have_content('Log out')
+      expect(page).to have_content('Invalid Email or password.')
+    end
   end
-
-  scenario 'can log out once logged in' do
-    visit '/'
-    sign_in_with @user
-    click_link 'Log out'
-    expect(page).to have_content('Signed out successfully')
-  end
-
-  scenario 'can view index posts without logging in' do
-    visit '/'
-    expect(page).to have_content('Test')
-  end
-
-  scenario 'cannot create a new post without logging in' do
-    visit '/'
-    expect(page).not_to have_content('Add a Snippet')
-  end
-
 end
