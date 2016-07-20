0a5fe9bae76d6794703c70817373c7b826877b29
Elena <elena.vilimaite@gmail.com>
2016-07-20 16:18:32 +0100

    reviews on profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,5 +2,6 @@ class UserController < ApplicationController
 	def index
 		@user = User.find_by(id: params[:id])
 		@snippets = @user.snippets
+		@reviews = @user.reviews.map { |r| Snippet.find_by(id: r.snippet_id)  }
 	end
 end
