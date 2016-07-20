2a891c258af953fcf166a2f62b1615e80adabda7
Elena <elena.vilimaite@gmail.com>
2016-07-20 14:09:04 +0100

    attempting something

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root to: 'snippets#index'
 	resources :snippets
-	match 'users/:id', :to => "user#index", :via => :get
+	match 'users/:id', :to => "user#index", :via => :get, as: 'user'
 end
