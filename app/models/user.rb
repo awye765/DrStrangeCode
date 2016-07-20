49fa62f1caf71ea08ddbfea622a5d7e057e446f8
lomlo <michael.lo@live.co.uk>
2016-07-20 10:53:30 +0100

    refactored the nested if statement conditionally showing edit and delete buttons in snippets#show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   validates :user_name, presence: true, length: {minimum: 4, maximum: 16}
 
   has_many :snippets, dependent: :destroy
+
+  def self.ownsSnippet?(snippet, current_user)
+    snippet.user.id === current_user.id
+  end
 end
