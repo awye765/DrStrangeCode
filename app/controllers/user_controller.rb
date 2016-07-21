39e8ea1596da19262a3bdcbd99e526cb407ff0d3
Elena <elena.vilimaite@gmail.com>
2016-07-21 09:22:16 +0100

    profiles done

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,6 @@ class UserController < ApplicationController
 	def index
 		@user = User.find_by(id: params[:id])
 		@snippets = @user.snippets
-		@reviews = @user.reviews.map { |r| Snippet.find_by(id: r.snippet_id)  }
+		@reviews = @user.reviews.map { |r| Snippet.find_by(id: r.snippet_id) }
 	end
 end
